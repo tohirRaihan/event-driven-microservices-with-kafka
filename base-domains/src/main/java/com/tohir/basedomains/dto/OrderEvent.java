2a58be909c07370828b8e1fb4484699127d7b359
Tohir Raihan <tohir.raihan@gmail.com>
2024-04-10 00:14:39 +0600

    :pencil2: Add: OrderEvent dto

--- base-domains/src/main/java/com/tohir/basedomains/dto/OrderEvent.java
+++ base-domains/src/main/java/com/tohir/basedomains/dto/OrderEvent.java
@@ -7,9 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
-    private String orderId;
-    private String name;
-    private int qty;
-    private double price;
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
 }
