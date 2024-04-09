5b9796aa23f63b1d09ce4c166849b844b04b79b6
Tohir Raihan <tohir.raihan@gmail.com>
2024-04-09 23:39:22 +0600

    :pencil2: Add: microservices added

---
+++ base-domains/src/test/java/com/tohir/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tohir.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
