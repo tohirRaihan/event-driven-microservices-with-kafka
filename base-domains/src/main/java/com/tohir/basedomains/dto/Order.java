8b4f5383f080cc2d46ce313740b4066e42aa9b55
Tohir Raihan <tohir.raihan@gmail.com>
2024-04-10 00:13:11 +0600

    :pencil2: Add: Order dto

---
+++ base-domains/src/main/java/com/tohir/basedomains/dto/Order.java
@@ -0,0 +1,17 @@
+package com.tohir.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    private String orderId;
+    private String name;
+    private int qty;
+    private double price;
+    
+}
