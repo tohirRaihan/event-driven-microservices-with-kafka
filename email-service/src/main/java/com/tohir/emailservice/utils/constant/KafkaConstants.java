b9b3efac5da05c057da027009e74c914a98eaed2
Tohir Raihan <tohir.raihan@gmail.com>
2024-04-10 16:39:13 +0600

    :pencil2: Add: KafkaConstants

---
+++ email-service/src/main/java/com/tohir/emailservice/utils/constant/KafkaConstants.java
@@ -0,0 +1,9 @@
+package com.tohir.emailservice.utils.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class KafkaConstants {
+    public static final String TOPIC_ORDERS = "orders";
+}
